fbc4b89368f4ca30d46dc5b647b848a5df1fa55e
EduardoDoblas <151891075+EduardoDoblas@users.noreply.github.com>
2023-11-24 19:00:43 -0600

    sexo en el oxxo

--- conteo_ventas.php
+++ conteo_ventas.php
@@ -1,31 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
-    <title>CAPRI - Historia</title>
+    <title>CAPRI - Ventas</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="Free Website Template" name="keywords">
     <meta content="Free Website Template" name="description">
+
     <!-- Favicon -->
     <link href="img/favicon.ico" rel="icon">
+
     <!-- Google Font -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"> 
+
     <!-- Font Awesome -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+
     <!-- Libraries Stylesheet -->
     <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    
     <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+
     <!-- Customized Bootstrap Stylesheet -->
     <link href="css/style.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    
+    
+    
     <link rel="stylesheet" href="css/">
 </head>
+
 <body>
     <!-- Navbar Start -->
     <div class="container-fluid p-0 nav-bar">
-    <nav class="navbar navbar-expand-lg bg-none navbar-dark py-3">
+        <nav class="navbar navbar-expand-lg bg-none navbar-dark py-3">
             <a href="index.html" class="navbar-brand px-lg-4 m-0">
                 <img src="img/LogoCapri.png" alt="logoinicio" width="240px">
             </a>
@@ -58,6 +69,7 @@
         </nav>
     </div>
     <!-- Navbar End -->
+
     <!-- Carousel Start -->
     <div class="container-fluid p-0 mb-5">
         <div id="blog-carousel" class="carousel slide overlay-bottom" data-ride="carousel">
@@ -66,11 +78,11 @@
                 <div class="carousel-item active">
                     <img class="w-100" src="img/bg.jpg" alt="Image" height="450px"  width="700px">
                     <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
-                        <h1 class="display-4 mb-3 mt-0 mt-lg-5 text-white text-uppercase">Acerca de Nosotros</h1>
+                        <h1 class="display-4 mb-3 mt-0 mt-lg-5 text-white text-uppercase">Conteo de Ventas</h1>
                         <div class="d-inline-flex mb-lg-5">
                             <p class="m-0 text-white"><a class="text-white" href="index.html">Inicio</a></p>
                             <p class="m-0 text-white px-2">/</p>
-                            <p class="m-0 text-white">Historia</p>
+                            <p class="m-0 text-white">Ventas</p>
                         </div>
                     </div>
                 </div>
@@ -78,51 +90,45 @@
         </div>
     </div>
     <!-- Carousel End -->
-
-<!-- About Start -->
-<div class="container-fluid py-5">
-    <div class="container">
-        <div class="section-title">
-            <h4 class="text-primary text-uppercase" style="letter-spacing: 5px; font-size: 2rem;">Acerca de Nosotros</h4>
-            <h1 class="display-4" style="font-size: 3rem;">Sirviendo desde 1950</h1>
-        </div>
-        <div id="info" class="row">
-            <div class="col-lg-4 py-5 py-lg-0">
-                <div class="position-relative h-100">
-                    <img class="position-absolute w-100 h-100" src="img/about.jpg" style="object-fit: cover;">
+    <div class="container-fluid py-5">
+        
+            <div class="section-title">
+                <h1 class="display-4">Formulario de Conteo de Ventas</h1>
+            </div>
+            <div id="info">
+                <div class="row">
+                    <div class="col-lg-4 py-0 py-lg-5">
+                        <h3 class="mb-3">Sabor del Helado:</h3>
+                        <h3 class="mb-3">Cantidad Vendida:</h3>
+                        <h3 class="mb-3">Precio por unidad:</h3>
+                        <h3 class="mb-3">Total de ventas:</h3>
+                        
+                    </div>
+                    <div class="col-lg-4 py-0 py-lg-5">
+                        <h3 class="mb-3">Mango</h3>
+                        <h3 class="mb-3">20</h3>
+                        <h3 class="mb-3">$65</h3>
+                        <h3 class="mb-3">$1,300</h3>
+                        
+                    </div>
                 </div>
             </div>
-            <div class="col-lg-4 py-0 py-lg-5">
-                <h1 class="mb-3">Filosofía</h1>
-                <h5 class="mb-3">Creer en estos principios y aplicarlos es importante, ya que son la base para el éxito de su proyecto</h5>
-                <p>
-                    Nevería Capri se ha mantenido en el gusto de los consumidores por más de 40 años, esto se debe a la excelente calidad de sus productos y a su servicio.
-                    Dentro de la industria de servicios alimenticios, el trato al cliente juega un papel prepoderante, es por eso que dentro del presente manual se ha incluido un
-                    capítulo exclusivo para este rubro, dentro de la filosofia de la empresa, la actitud y disposición de todo el equipo que participa en la operación para brindar un servicio
-                    excelente a los clientes, debe ser de lo mejor, brindar un excelente servicio debe ser una convicción y un objetivo que diariamente distinga la operación de nuestra unidad.
-                </p>
-                
-            </div>
-            <div class="col-lg-4 py-0 py-lg-5">
-                <h1 class="mb-3">Nuestra Visión</h1>
-                <p>Ser la empresa líder, la más grande y competitiva en el ramo de la nieve raspada, distinguiéndose así por 
-                    tener el mejor personal capacitado en servicio al cliente, el mejor producto, los sistemas de higiene más
-                    seguros, respetar su medio ambiente y contar con los mejores recursos técnicos del ramo. Para proyectar su
-                    imagen nacional e internacionalmente
-                </p>
-                <h5 class="mb-3"><i class="fa fa-check text-primary mr-3"></i>La mejor atención al cliente.</h5>
-                <h5 class="mb-3"><i class="fa fa-check text-primary mr-3"></i>Mejor sistema de higiene.</h5>
-                <h5 class="mb-3"><i class="fa fa-check text-primary mr-3"></i>Cuidado al medio ambiente.</h5>
-                
-            </div>
-        </div>
+        
     </div>
-</div>
-<!-- About End -->
+    <!-- About Start -->
+    
 
-    <!-- Footer Start -->
+
+
+
+
+
+    <!-- About End -->
+
+
+   <!-- Footer Start -->
     <div class="container-fluid footer text-white mt-5 pt-5c px-0 position-relative overlay-top">
-    <div class="row mx-0 pt-5 px-sm-3 px-lg-5 mt-4">
+        <div class="row mx-0 pt-5 px-sm-3 px-lg-5 mt-4">
             <div class="col-lg-3 col-md-6 mb-5">
                 <h4 class="text-white text-uppercase mb-4" style="letter-spacing: 3px;">Visítanos</h4>
                 <p><i class="fa fa-map-marker-alt mr-2"></i>Calz Paseo de la Rosita 610-Int. A, Residencial Campestre 
@@ -166,6 +172,8 @@
 
     <!-- Back to Top -->
     <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="fa fa-angle-double-up"></i></a>
+
+
     <!-- JavaScript Libraries -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
@@ -175,10 +183,13 @@
     <script src="lib/tempusdominus/js/moment.min.js"></script>
     <script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
     <script src="lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+
     <!-- Contact Javascript File -->
     <script src="mail/jqBootstrapValidation.min.js"></script>
     <script src="mail/contact.js"></script>
+
     <!-- Template Javascript -->
     <script src="js/main.js"></script>
 </body>
+
 </html>
