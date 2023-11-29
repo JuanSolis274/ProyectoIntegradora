86c2a2889d487ad7bb0c32ff91b65ba148b062fc
tiogilipollas21 <Luiscarlosfernan67@gmail.com>
2023-11-29 00:43:22 -0600

    ignorar

--- css_login/login_usuario_be.php
+++ css_login/login_usuario_be.php
@@ -2,7 +2,7 @@
 
   session_start();
 
-  include 'conexion_be.php';
+  //include 'conexion_be.php';//
   
   $correo = $_POST['correo'];
   $contrasena = $_POST['contrasena'];
@@ -10,11 +10,13 @@
   //encriptamiento//
   $contrasena = hash('sha512', $contrasena); 
 
-  $validar_login = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo='$correo' 
-  and contrasena='$contrasena'");
+  $consulta= "SELECT * FROM usuarios WHERE correo='$correo' and contrasena='$contrasena'";
+  $resultado=mysqli_query($conexion, $consulta );
+
+  $filas=mysqli_fetch_array($resultado);
 
 //Inicio Sesion//
-  if(mysqli_num_rows($validar_login) > 0){
+  if($filas['id_car']==0){
     $_SESSION['usuario'] = $correo; 
 
     header ("location: ../Shop.php");
