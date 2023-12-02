1fa7ea145d361577535388890c6a012f0af52498
tiogilipollas21 <Luiscarlosfernan67@gmail.com>
2023-12-01 21:14:01 -0600

    loogin
    100 porciento terminado el login

--- css_login/login_usuario_be.php
+++ css_login/login_usuario_be.php
@@ -1,50 +1,70 @@
 <?php
-
   session_start();
-
   include 'conexion_be.php';
+
   
   $correo = $_POST['correo'];
   $contrasena = $_POST['contrasena'];
 
+  $correo_consulta = null;
+  $contra_consulta = null;
+  $rol = null;
+
   //encriptamiento//
   $contrasena = hash('sha512', $contrasena); 
+
 
   $validar_login = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo='$correo' 
   and contrasena='$contrasena'");
 
- $filas = mysqli_fetch_array($validar_login);
-
-//Inicio Sesion//
-  if(mysqli_num_rows($validar_login) > 0){
-    $_SESSION['usuario'] = $correo; 
-
-    header ("location: ../Shop.php");
-    exit();
-
-  }elseif (mysqli_num_rows($validar_login) < 1){
-    
-   
-
-    header ("location: ../index_admin.php");
-
-  }else{
-     '
-     <script>
-     
-     alert("Inicio  Fallido, Intentalo de nuevo");
-     window.location = "../usuario.php";
-     
-     
-     </script>
-     
-     
-     
-     ';
+  foreach($validar_login as $res)
+  {
+    $correo_consulta = $res['correo'];
+    $contra_consulta = $res['contrasena'];
+    $rol = $res['id_car'];
 
 
+    echo $res['correo'];
   }
 
-?>
+  // echo $correo_consulta.'    ';
+  // echo $contra_consulta.'    ';
+  // echo $rol;
 
+  if($correo === $correo_consulta && $contrasena === $contra_consulta)
+  {
+    if($rol == 0)
+    {
+      $_SESSION['usuario'] = $correo;
+      header('Location: ../Shop.php?rol='.$rol);
+    }
+    if($rol == 1)
+    {
+      $_SESSION['usuario'] = $correo;
+      header('Location: ../index_admin.php');
+    }
+  }
+  else
+  {
+    echo'
+         <script>
+             alert("Usuario No Existe, Por Favor Verifique Los Datos Introduccion");
+             window.location = "../usuario.php";  
+    
+    
+    
+           </script>
+    
+           
+    ';
+
+    exit();
+  }
+
+
+  
+
+
+
+  mysqli_close($conexion);
 ?>
